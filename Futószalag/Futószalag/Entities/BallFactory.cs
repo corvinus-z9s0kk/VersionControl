3326870b11a33b0d5f214fc193d7fb0ddfe2c0ed
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-11-12 18:24:43 +0100

    Labdagyár létrehozása

---
+++ Futószalag/Futószalag/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futószalag.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
