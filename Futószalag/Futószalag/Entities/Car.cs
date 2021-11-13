15c25d7a63225c31c88b93c15ef362db465335a0
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-11-13 10:49:34 +0100

    Autó létrehozása

---
+++ Futószalag/Futószalag/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Futószalag.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futószalag.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Image/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
--- Futószalag/Futószalag/Form1.cs
+++ Futószalag/Futószalag/Form1.cs
@@ -42,7 +42,7 @@ namespace Futószalag
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition) maxPosition = ball.Left;
             }
 
