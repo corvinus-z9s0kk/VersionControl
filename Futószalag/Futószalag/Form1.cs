99b28fa7df2d89b04416ff1040c841af9f901a3c
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-11-13 11:00:13 +0100

    Absztrakció felhasználása a futószalagban

--- Futószalag/Futószalag/Form1.cs
+++ Futószalag/Futószalag/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Futószalag.Entities;
+﻿using Futószalag.Abstractions;
+using Futószalag.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,10 +15,10 @@ namespace Futószalag
     public partial class Form1 : Form
     {
 
-        List<Ball> _balls = new List<Ball>();
+        List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -31,26 +32,26 @@ namespace Futószalag
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveToy();
-                if (ball.Left > maxPosition) maxPosition = ball.Left;
+                toy.MoveToy();
+                if (toy.Left > maxPosition) maxPosition = toy.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
-                mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                var oldestToy = _toys[0];
+                mainPanel.Controls.Remove(oldestToy);
+                _toys.Remove(oldestToy);
             }
         }
 
