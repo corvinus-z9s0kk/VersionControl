cae8a2dab5ac2e75e422193040a221c266a68af5
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-11-13 10:55:47 +0100

    Absztrakció felhasználása a gyárakban

--- Futószalag/Futószalag/Entities/CarFactory.cs
+++ Futószalag/Futószalag/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Futószalag.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
