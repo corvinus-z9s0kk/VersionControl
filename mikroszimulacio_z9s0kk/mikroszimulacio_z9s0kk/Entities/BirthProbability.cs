f0c8a088a4e869ca935d1dbc3fe8f4475c902184
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-11-30 19:49:34 +0100

    Osztályok létrehozása

---
+++ mikroszimulacio_z9s0kk/mikroszimulacio_z9s0kk/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikroszimulacio_z9s0kk.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public byte NumberOfChlidren { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ mikroszimulacio_z9s0kk/mikroszimulacio_z9s0kk/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikroszimulacio_z9s0kk.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public byte NumberOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
