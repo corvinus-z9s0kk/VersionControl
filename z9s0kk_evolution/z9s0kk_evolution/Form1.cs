51d4f4f893d9a136004de5dd4f8e89f8fcb81cd3
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-12-08 19:01:06 +0100

    Játéktér

--- z9s0kk_evolution/z9s0kk_evolution/Form1.cs
+++ z9s0kk_evolution/z9s0kk_evolution/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace z9s0kk_evolution
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
     }
 }
